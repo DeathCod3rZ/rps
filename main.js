02a0e8e0c38944feb56d52e41350d8e889606bbb
DeathCod3rZ <keanefoo@gmail.com>
2024-11-05 21:43:17 +0800

    Update main.js
    added gethuman choice functions and renamed choiceNum to computerChoice

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 console.log('Hello World!');
-
+const rpsArr = ["rock","paper","scissors"];
+let HumanChoice = "";
 function getComputerChoice (){
-  const rpsArr = ["rock","paper","scissors"];
-  let choiceNum = Math.floor(Math.random()*3);
+  
+  let computerChoice = Math.floor(Math.random()*3);
   return rpsArr[choiceNum];
 }
 
+function getHumanChoice(){
+  while(!rpsArr.includes(HumanChoice)){
+    HumanChoice = prompt("Enter rock, paper, or scissors");
+    if(!rpsArr.includes(HumanChoice)){
+      alert("Select either rock, paper, or scissors only")
+    }
+  }
+  return HumanChoice;
+
+
+
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
