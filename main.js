65932b534e3a2f51a3affe80f0220a8112300ce6
DeathCod3rZ <keanefoo@gmail.com>
2024-11-05 21:04:01 +0800

    Update main.js
    add getComputerChoice function

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 console.log('Hello World!');
 
 function getComputerChoice (){
-  let strArr = ['rock','paper];
+  const rpsArr = ["rock","paper","scissors"];
+  let choiceNum = math.floor(math.random()*3)+1;
+  return rpsArr[choiceNum];
 }
+
+getComputerChoice();
